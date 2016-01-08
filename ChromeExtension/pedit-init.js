acd18fec09e7fe45498df277b7dced18a452265d
Pranay Prakash <pranay.gp@gmail.com>
2016-01-08 18:44:05 +0400

    Cleanup
    Removed url and path variable declerations in core pedit scripts

--- ChromeExtension/pedit-init.js
+++ ChromeExtension/pedit-init.js
@@ -2,8 +2,6 @@ var dataHandle = new Firebase("http://pedit.firebaseio.com/");
 var extensionId = chrome.runtime.id;
 
 var host = u2k(document.location.hostname);
-var path = u2k(document.location.pathname.substr(1));
-var url = host + "/" + path;
 function u2k(inputURL){
     var url = inputURL;
     url = url.replace(/\./g,'(dot)');
--- ChromeExtension/pedit-save.js
+++ ChromeExtension/pedit-save.js
@@ -1,6 +1,4 @@
 var host = u2k(document.location.hostname);
-var path = u2k(document.location.pathname.substr(1));
-var url = host + "/" + path;
 function u2k(inputURL){
     var url = inputURL;
     url = url.replace(/\./g,'(dot)');
