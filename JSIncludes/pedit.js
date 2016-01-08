af86a9c402fe1e0c21cdc105daa54bf3531256ca
Pranay Prakash <pranay.gp@gmail.com>
2016-01-08 18:17:20 +0400

    Load content by looping over local pedit tags, not firebase data

--- JSIncludes/pedit.js
+++ JSIncludes/pedit.js
@@ -13,11 +13,10 @@ function u2k(inputURL){
 var ref = new Firebase("http://pedit.firebaseio.com/");
 // Attach an asynchronous callback to read the data at our posts reference
 ref.child(host).child("content").child(path).on("value", function(snapshot) {
-    //  console.log(snapshot.val());
-    var data = snapshot.val()
-    for(var key in data){
-        $("[data-pedit='"+key+"']").html(data[key]);
-    }
+    var peditData = snapshot.val();
+    $("[data-pedit]").each(function(){
+        $(this).html(peditData[$(this).data("pedit")]);
+    });
 }, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
 });
